7d52e57244f4e54ecc04eb09f46f3f1269395b91
Izzy Swart <zenerboson@gmail.com>
2020-05-26 12:58:21 -0700

    fail by terminating stream

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::net::SocketAddr;
 
 pub use url::Url;
 
-pub trait Socket: Stream + TryStream<Ok = Vec<u8>> + Sink<Vec<u8>> {
+pub trait Socket: Stream + Stream<Item = Vec<u8>> + Sink<Vec<u8>> {
     type Close: TryFuture<Ok = ()>;
 
     fn close(self) -> Self::Close;
@@ -12,13 +12,11 @@ pub trait Socket: Stream + TryStream<Ok = Vec<u8>> + Sink<Vec<u8>> {
 
 pub trait SocketExt: Socket {
     type SendError;
-    type ReceiveError;
     type CloseError;
 }
 
 impl<T: Socket> SocketExt for T {
     type SendError = <Self as Sink<Vec<u8>>>::Error;
-    type ReceiveError = <Self as TryStream>::Error;
     type CloseError = <Self::Close as TryFuture>::Error;
 }
 
